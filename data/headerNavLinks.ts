3664011bbb28ada1611b0a410e3f120a8488808a
FlauFly <tirhhe@gmail.com>
2024-07-26 21:06:38 +0200

    favicons

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/blog', title: 'Essays' },
   { href: '/library', title: 'Library'},
+  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
