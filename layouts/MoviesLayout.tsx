f3b11e62f82a2394b5e5c02615c415df253d9fc4
FlauFly <tirhhe@gmail.com>
2024-07-26 19:15:56 +0200

    prettify fixes

--- layouts/MoviesLayout.tsx
+++ layouts/MoviesLayout.tsx
@@ -84,7 +84,9 @@ export default function ListLayout({
           <h1 className="mr-3 text-3xl font-medium uppercase text-primary-500 opacity-75 hover:text-primary-600 dark:hover:text-primary-400 dark:opacity-25">
             <Link href="/books">Books</Link>
           </h1>
-          <h1 className="mr-3 text-3xl font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">Movies</h1>
+          <h1 className="mr-3 text-3xl font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+            Movies
+          </h1>
         </div>
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
