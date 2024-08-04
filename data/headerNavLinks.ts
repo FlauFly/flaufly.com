5fe24be85d9402a31ddf2893dfe510a158108346
FlauFly <tirhhe@gmail.com>
2024-08-04 21:57:25 +0200

    readme start

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Essays' },
   { href: '/library', title: 'Library'},
+  { href: '/lib', title: 'Lib'},
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
